6a469f0b1459a799a640b40c549e2cba6f869809
Leonardo <62109173+leogbittante@users.noreply.github.com>
2023-07-19 14:31:06 -0300

    Add files via upload

---
+++ jogos/forca.py
@@ -0,0 +1,10 @@
+
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao jogo de Forca!")
+    print("*********************************")
+
+    print("FIM DE JOGO")
+
+if(__name__ == "__main__"):
+    jogar()
